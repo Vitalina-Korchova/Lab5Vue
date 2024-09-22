694f10d2eb769cfe069a2f62f35a7d6f3a59c038
VitalinaKorchova <vitalina.korchova@gmail.com>
2024-09-22 21:56:31 +0300

    created component

--- Users-app/src/App.vue
+++ Users-app/src/App.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import UserCard from './components/user-card.vue';
+import user1Photo from './assets/user1.jpg';
+import user2Photo from './assets/user2.png';
 
+interface User {
+  firstName: string;
+  lastName: string;
+  gender: string;
+  age: number;
+  position: string;
+  photo: string;
+  hobbies: string[];
+}
+
+const users = ref<User[]>([
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: 'Male',
+    age: 30,
+    position: 'Software Developer',
+    photo: user1Photo,
+    hobbies: ['Coding', 'Reading', 'Gaming'],
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    gender: 'Female',
+    age: 25,
+    position: 'Designer',
+    photo: user2Photo,
+    hobbies: ['Drawing', 'Traveling'],
+  }
+]);
 </script>
 
 <template>
- <h1>Hello</h1>
+<div class="user-container">
+    <UserCard v-for="(user, index) in users" :key="index" :user="user"></UserCard>
+</div>
 </template>
 
 <style >
---
+++ Users-app/src/components/user-card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface UserData {
+  firstName: string;
+  lastName: string;
+  gender: string;
+  age: number;
+  position: string;
+  photo: string;
+  hobbies: string[];
+}
+
+const props = defineProps<{
+  user: UserData
+}>();
+</script>
+
+<template>
+<div class="user-card">
+    <img :src="user.photo" alt="User Photo" class="user-photo" />
+    <h2 class="Name_Username">{{user.firstName }} {{ user.lastName }}</h2>
+    <p class="gender">{{ user.gender }}</p>
+    <p class="age">{{ user.age }}</p>
+    <p class="position">{{ user.position }}</p>
+    <p class="hobbies">{{ user.hobbies.join(', ') }}</p>
+</div>
+</template>
+
+<style scoped>
+.user-card {
+  display: flex;
+  align-items: center;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 400px;
+  margin: 20px auto;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.user-photo{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
