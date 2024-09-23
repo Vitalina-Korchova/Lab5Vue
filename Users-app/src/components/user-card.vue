f0aa443071f081c5dc90772b7f3771aa827a778a
VitalinaKorchova <vitalina.korchova@gmail.com>
2024-09-23 19:24:37 +0300

    finished

--- Users-app/src/components/user-card.vue
+++ Users-app/src/components/user-card.vue
@@ -15,32 +15,48 @@ const props = defineProps<{
 </script>
 
 <template>
-<div class="user-card">
+<div :class="{'young-user': user.age <= 18, 'adult-user': user.age > 18}" class="user-card">
     <img :src="user.photo" alt="User Photo" class="user-photo" />
-    <h2 class="Name_Username">{{user.firstName }} {{ user.lastName }}</h2>
-    <p class="gender">{{ user.gender }}</p>
-    <p class="age">{{ user.age }}</p>
-    <p class="position">{{ user.position }}</p>
-    <p class="hobbies">{{ user.hobbies.join(', ') }}</p>
+    <span class="name-username">{{ user.firstName }} {{ user.lastName }}</span>
+    <span class="gender">{{ user.gender }}</span>
+    <span class="position">{{ user.position }}</span>
+    <span v-for="hobby in user.hobbies" :key="hobby" class="hobbies">{{ hobby }}</span>
+    <span v-if="user.age > 18" class="age">{{ user.age }} y.o.</span>
 </div>
 </template>
 
 <style scoped>
 .user-card {
   display: flex;
+  flex-direction: column;
   align-items: center;
+  gap: 5px;
   background-color: #f5f5f5;
-  border: 1px solid #ccc;
   border-radius: 10px;
-  padding: 20px;
-  max-width: 400px;
+  padding: 10px;
+  width: 240px;
+  height: auto;
   margin: 20px auto;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
 .user-photo{
-  width: 50px;
-  height: 50px;
+  width: 80px;
+  height: 80px;
   border-radius: 50%;
+}
+
+.name-username{
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.young-user{
+  border: 1.5px solid rgb(27, 236, 229);
+}
+
+.adult-user{
+  border: 1.5px solid rgb(236, 149, 27);
 }
 </style>
